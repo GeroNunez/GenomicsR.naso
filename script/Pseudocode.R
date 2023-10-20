dd6b7f2484b643fe528b66703908f336ddc6845c
GeroNunez <gnunezg@puce.edu.ec>
2023-10-20 18:55:49 -0500

    update

---
+++ script/Pseudocode.R
@@ -0,0 +1,11 @@
+# Escribe un código que imprima un número y su cuadrado a lo largo de un rango de valores
+# Tambíen que imprima la suma de todos los cuadrados de dicho rango 
+
+# Define el valor mínimo y el máximo
+
+# Crea una variable squaresum que tenga al inicio 0
+
+# Haz un loop a lo largo de ese rango y por cada valor: 
+	# Imprime el valor y valor al cuadrado
+	# Adiciona el valor al cuadrado a la varible squaresum
+# Una vez terminado el loop imprime la variable squaresum
